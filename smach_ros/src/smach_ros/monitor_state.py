8c2a27f2292ef10601ac93d27bd29a7a37909e47
Bruno Lacerda <bruno@cca-0830.(none)>
2013-07-30 19:43:27 +0100

    edited monitor state to allow input and output keys

--- smach_ros/src/smach_ros/monitor_state.py
+++ smach_ros/src/smach_ros/monitor_state.py
@@ -12,12 +12,12 @@ __all__ = ['MonitorState']
 class MonitorState(smach.State):
     """A state that will check a given ROS topic with a condition function. 
     """
-    def __init__(self, topic, msg_type, cond_cb, max_checks=-1):
-        smach.State.__init__(self,outcomes=['valid','invalid','preempted'])
+    def __init__(self, topic, msg_type, cond_cb, max_checks=-1,input_keys = [],output_keys=[]):
+        smach.State.__init__(self,outcomes=['valid','invalid','preempted'], input_keys = input_keys,output_keys = output_keys)
 
         self._topic = topic
         self._msg_type = msg_type
-        self._cond_cb = cond_cb
+        self._cond_cb = cond_cb                    
         self._max_checks = max_checks
         self._n_checks = 0
 
@@ -26,7 +26,7 @@ class MonitorState(smach.State):
     def execute(self, ud):
         self._n_checks = 0
 
-        self._sub = rospy.Subscriber(self._topic, self._msg_type, self._cb, callback_args=[ud])
+        self._sub = rospy.Subscriber(self._topic, self._msg_type, self._cb, callback_args=ud)
 
         self._trigger_cond.acquire()
         self._trigger_cond.wait()
@@ -43,7 +43,7 @@ class MonitorState(smach.State):
 
         return 'invalid'
 
-    def _cb(self, msg, ud):
+    def _cb(self,msg,ud) :
         self._n_checks += 1
         try:
             if (self._max_checks > 0 and self._n_checks >= self._max_checks) or not self._cond_cb(ud, msg):
