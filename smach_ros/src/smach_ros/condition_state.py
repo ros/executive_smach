6b1b5df218258219317d65952cc6081a42c626a5
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-03-26 12:13:48 +0100

    rclpy time interface

--- smach_ros/src/smach_ros/condition_state.py
+++ smach_ros/src/smach_ros/condition_state.py
@@ -1,6 +1,6 @@
-
-import roslib; roslib.load_manifest('smach_ros')
-import rospy
+import rclpy
+import rclpy.time
+import rclpy.clock
 
 import threading
 import traceback
@@ -11,17 +11,19 @@ __all__ = ['ConditionState']
 
 class ConditionState(smach.State):
     """A state that will check a condition function a number of times.
-    
+
     If max_checks > 1, it will block while the condition is false and once it
     has checked max_checks times, it will return false.
     """
     def __init__(self,
+            node,
             cond_cb,
             input_keys = [],
-            poll_rate = rospy.Duration(0.05),
+            poll_rate = rclpy.time.Duration(seconds=0.05),
             timeout = None,
             max_checks = 1):
         smach.State.__init__(self,outcomes = ['true', 'false','preempted'], input_keys = input_keys)
+        self._node = node
 
         self._cond_cb = cond_cb
         if hasattr(cond_cb,'get_registered_input_keys') and hasattr(cond_cb,'get_registered_output_keys'):
@@ -34,12 +36,14 @@ class ConditionState(smach.State):
         self._max_checks = max_checks
 
     def execute(self, ud):
-        start_time = rospy.Time.now()
+        clock = rclpy.clock.ROSClock()
+        rate = self._node.create_rate(1 / self._poll_rate, clock)
+        start_time = clock.now()
         n_checks = 0
 
         while self._max_checks == -1 or n_checks <= self._max_checks:
             # Check for timeout
-            if self._timeout and rospy.Time.now() - start_time > self._timeout:
+            if self._timeout and clock.now() - start_time > self._timeout:
                 break
             # Check for preemption
             if self.preempt_requested():
@@ -52,6 +56,6 @@ class ConditionState(smach.State):
             except:
                 raise smach.InvalidUserCodeError("Error thrown while executing condition callback %s: " % str(self._cond_cb) +traceback.format_exc())
             n_checks += 1
-            rospy.sleep(self._poll_rate)
-        
+            rate.sleep()
+
         return 'false'
