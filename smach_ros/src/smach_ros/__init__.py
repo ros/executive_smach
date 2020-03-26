2121c848962a6248ce383b575d58272e887cc4d1
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-03-26 12:13:32 +0100

    use rclpy logger

--- smach_ros/src/smach_ros/__init__.py
+++ smach_ros/src/smach_ros/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2010, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -25,8 +25,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 
 # ROS imports
-import roslib; roslib.load_manifest('smach_ros')
-import rospy
+import rclpy
 
 # SMACH
 import smach
@@ -41,12 +40,12 @@ __all__ = ['set_preempt_handler',
 
 # Setup smach-ros interface
 smach.set_loggers(
-        rospy.loginfo,
-        rospy.logwarn,
-        rospy.logdebug,
-        rospy.logerr)
+        rclpy.get_logger(__name__).info,
+        rclpy.get_logger(__name__).warn,
+        rclpy.get_logger(__name__).debug,
+        rclpy.get_logger(__name__).error)
 
-smach.set_shutdown_check(rospy.is_shutdown)
+smach.set_shutdown_check(rclpy.ok)
 
 ### Core classes
 from util import set_preempt_handler
