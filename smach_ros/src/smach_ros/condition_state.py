66540ee5cc41628f89f8fea4e0b0725180da09cc
Charles Lesire-Cabaniols <charles.lesire@onera.fr>
2020-03-27 16:26:48 +0100

    ConditionState are RosState

--- smach_ros/src/smach_ros/condition_state.py
+++ smach_ros/src/smach_ros/condition_state.py
@@ -6,10 +6,11 @@ import threading
 import traceback
 
 import smach
+from .ros_state import RosState
 
 __all__ = ['ConditionState']
 
-class ConditionState(smach.State):
+class ConditionState(RosState):
     """A state that will check a condition function a number of times.
 
     If max_checks > 1, it will block while the condition is false and once it
@@ -19,11 +20,10 @@ class ConditionState(smach.State):
             node,
             cond_cb,
             input_keys = [],
-            poll_rate = rclpy.time.Duration(seconds=0.05),
+            poll_rate = 20,
             timeout = None,
             max_checks = 1):
-        smach.State.__init__(self,outcomes = ['true', 'false','preempted'], input_keys = input_keys)
-        self._node = node
+        RosState.__init__(self, node, outcomes = ['true', 'false','preempted'], input_keys = input_keys)
 
         self._cond_cb = cond_cb
         if hasattr(cond_cb,'get_registered_input_keys') and hasattr(cond_cb,'get_registered_output_keys'):
@@ -36,14 +36,13 @@ class ConditionState(smach.State):
         self._max_checks = max_checks
 
     def execute(self, ud):
-        clock = rclpy.clock.ROSClock()
-        rate = self._node.create_rate(1 / self._poll_rate, clock)
-        start_time = clock.now()
+        rate = self.node.create_rate(1 / self._poll_rate)
+        start_time =  self.node.get_clock().now()
         n_checks = 0
 
         while self._max_checks == -1 or n_checks <= self._max_checks:
             # Check for timeout
-            if self._timeout and clock.now() - start_time > self._timeout:
+            if self._timeout and  self.node.get_clock().now() - start_time > self._timeout:
                 break
             # Check for preemption
             if self.preempt_requested():
