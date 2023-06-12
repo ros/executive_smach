967a5610d5c86fd1a39e50465d68d73558b136a3
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2023-06-12 15:53:53 -0400

    3.0.1

--- smach_ros/setup.py
+++ smach_ros/setup.py
@@ -7,7 +7,7 @@ package_name = 'smach_ros'
 
 setup(
     name=package_name,
-    version='3.0.0',
+    version='3.0.1',
     packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
