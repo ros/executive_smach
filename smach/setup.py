2fc3fb18bb8729f154919b1f5952e7df4d4bc56b
Johannes Kübel <johannes.kubel@gmail.com>
2026-04-23 19:27:50 +0900

    Fix build warnings

--- smach/setup.py
+++ smach/setup.py
@@ -13,12 +13,7 @@ setup(
     zip_safe=True,
     maintainer='Isaac I. Y. Saito',
     maintainer_email='gm130s@gmail.com',
-    description='''
-        SMACH is a task-level architecture for rapidly creating complex robot
-        behavior. At its core, SMACH is a ROS-independent Python library to build
-        hierarchical state machines. SMACH is a new library that takes advantage of
-        very old concepts in order to quickly create robust robot behavior with
-        maintainable and modular code.''',
+    description='SMACH is a task-level architecture for rapidly creating complex robot behavior. At its core, SMACH is a ROS-independent Python library to build hierarchical state machines. SMACH is a new library that takes advantage of very old concepts in order to quickly create robust robot behavior with maintainable and modular code.',
     license='BSD',
     data_files=[
         ('share/ament_index/resource_index/packages',
